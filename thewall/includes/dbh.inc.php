36d7ddc03e714aa71c1dbead44cc004fcc4eea02
Vadim Pisa <27266@ma-web.nl>
2019-11-08 11:01:29 +0100

    Add files via upload

---
+++ thewall/includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php 
+
+$servername = "127.0.0.1"; // Als we hosten op domein moet het anders natuurlijk!!
+$username = "c3687thewall";
+$password = "KQvhtfP65_";
+$dbname = "c3687thewall";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
